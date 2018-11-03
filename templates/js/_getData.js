7b311222bf251f1f569331994858d5d662da06ef
tobuk <yesmeyou97@gamil.com>
2018-11-04 07:45:33 +0900

    anotherj

--- templates/js/_getData.js
+++ templates/js/_getData.js
@@ -1,11 +1,9 @@
 function getData(txID){
 	// txID가 0 이면 전체 조회
-	var formData = {
-            'txID' : 0
-        }
-
-	$.ajax({ 
-	    type: "POST", 
+	var formData = 'mode=getMsg&txID='+txID;
+/*
+	$.ajax({
+	    type: "POST",
 	    url: "http://54.169.253.56/?mode=getMsg",
 	    data: formData,
 	    success: function(response){
@@ -14,4 +12,26 @@ function getData(txID){
 	    error: function(request, status, error){
 	    },
 	})
+	*/
+
+	$.ajax({
+		url: "http://54.169.253.56/",
+		contentType: 'application/json; charset=utf-8',
+		crossDomain: true,
+		dataType: 'jsonp',
+		method : "POST",
+		data : formData,
+		success: function (data, textStatus, jqXHR) {
+			console.log(data);
+			console.log(textStatus);
+			console.log(jqXHR);
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			console.log(jqXHR);
+			console.log(textStatus);
+			console.log(errorThrown);
+
+		}
+	});
+
 }
