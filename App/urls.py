72417c2bbc5c15d390d8aeb67cd9adb59c9d8057
wkddnjset <wkddnjset@naver.com>
2018-11-03 23:43:48 +0900

    배포 하자

--- App/urls.py
+++ App/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import MainPage
+from .views import MainPage, SecondPage, ThirdPage
 
 app_name = "App"
 urlpatterns = [
     path('', MainPage, name="home"),
+    path('second', SecondPage, name="second"),
+    path('third', ThirdPage, name="third"),
+
 ]
--- App/views.py
+++ App/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def MainPage(request):
     return render(request, 'index.html', {})
+
+def SecondPage(request):
+    return render(request, 'index_2.html', {})
+
+def ThirdPage(request):
+    return render(request, 'index_3.html', {})
+
