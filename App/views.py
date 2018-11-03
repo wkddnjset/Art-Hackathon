c11bfa9bdb0862e4317732ef78b1d6796cf989d4
wkddnjset <wkddnjset@naver.com>
2018-11-04 00:26:27 +0900

    개발 중입니다.

--- App/views.py
+++ App/views.py
@@ -3,11 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def MainPage(request):
-    return render(request, 'index.html', {})
-
-def SecondPage(request):
-    return render(request, 'index_2.html', {})
-
-def ThirdPage(request):
-    return render(request, 'index_3.html', {})
-
+    return render(request, 'Main.html', {})
---
+++ templates/js/init.js
@@ -0,0 +1,62 @@
+function init() {
+	container = document.createElement( 'div' )
+	document.body.appendChild( container )
+    // scene
+	scene = new THREE.Scene()
+	scene.background = new THREE.Color( 0xcce0ff )
+	scene.fog = new THREE.Fog( 0xcce0ff, 400, 10000 )
+	// camera
+	camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 10000 )
+	camera.position.set( 1000, 1100, 2000 )
+
+	// lights
+	scene.add( new THREE.AmbientLight( 0x666666 ) )
+	var light = new THREE.DirectionalLight( 0xdfebff, 1 )
+	light.position.set( 200, 120, 80 )
+	light.position.multiplyScalar( 1 )
+	light.castShadow = true
+	light.shadow.mapSize.width = 1024
+	light.shadow.mapSize.height = 1024
+	var d = 1000
+	light.shadow.camera.left = - d
+	light.shadow.camera.right = d
+	light.shadow.camera.top = d
+	light.shadow.camera.bottom = - d
+	light.shadow.camera.far = 5000
+	scene.add( light )
+	// Ground
+	ground()
+	// Tree
+	tree(scale=0.5, mesh_name="tree_1", x=100, z=100)
+	tree(scale=0.2, mesh_name="tree_3", x=-20, z=170)
+	tree(scale=0.4, mesh_name="tree_4", x=50, z=-100)
+	tree(scale=1, mesh_name="tree_2", x=0, z=0)
+
+	// Butterfly
+	butterfly_1 = butterfly("butterfly_1")
+	scene.add( butterfly_1 )
+
+	// render
+	renderer = new THREE.WebGLRenderer( { antialias: true } )
+	renderer.setPixelRatio( window.devicePixelRatio )
+	renderer.setSize( window.innerWidth, window.innerHeight )
+	container.appendChild( renderer.domElement )
+	renderer.gammaInput = true
+	renderer.gammaOutput = true
+	renderer.shadowMap.enabled = true
+	// controls
+	var controls = new THREE.OrbitControls( camera, renderer.domElement )
+	controls.maxPolarAngle = Math.PI * 0.6
+	controls.minDistance = 1000
+	controls.maxDistance = 5000
+	// performance monitor
+	// stats = new Stats()
+	// container.appendChild( stats.dom )
+	//
+	document.addEventListener( 'mousedown', onDocumentMouseDown, false )
+
+	projector = new THREE.Projector();
+
+	window.addEventListener( 'resize', onWindowResize, false )
+
+}
