ea41b1148f5460a173b4c760c996a4a407409e18
wkddnjset <wkddnjset@naver.com>
2018-11-04 00:28:10 +0900

    디렉토리 구조화

--- App/urls.py
+++ App/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import MainPage, SecondPage, ThirdPage
+from .views import MainPage
 
 app_name = "App"
 urlpatterns = [
     path('', MainPage, name="home"),
-    path('second', SecondPage, name="second"),
-    path('third', ThirdPage, name="third"),
-
 ]
